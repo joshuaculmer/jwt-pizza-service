38e0fc63c25680531d42cd88bf2e64818b9f4c40
joshuaculmer <joshuadculmer@gmail.com>
2025-11-07 11:22:49 -0700

    Typo in orderRouter

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -140,14 +140,14 @@ orderRouter.post(
     if (r.ok) {
       res.send({ order, followLinkToEndChaos: j.reportUrl, jwt: j.jwt });
       console.log(order);
-      metrics.pizzaPurchase(true, 2, 500, 0.001);
+      metric.pizzaPurchase(true, 2, 500, 0.001);
     } else {
       res.status(500).send({
         message: "Failed to fulfill order at factory",
         followLinkToEndChaos: j.reportUrl,
       });
       console.log(order);
-      metrics.pizzaPurchase(false, 2, 5000, 0.001);
+      metric.pizzaPurchase(false, 2, 5000, 0.001);
     }
   })
 );
